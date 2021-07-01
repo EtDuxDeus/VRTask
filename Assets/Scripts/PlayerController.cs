55b9840e6fbb9ed02529b46f0d5ed4a9589649c6
EtDuxDeus <minerukraine@gmail.com>
2021-07-01 16:49:22 +0300

    Feat(Scripts) Add Player Controller and created scene with teleporters position's and object's to destroy
    -add aim pointer in the middle of the screen
    -add object's for teleportations
    -add script to controll teleportation and destroing aimed object's

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Tools
+{
+	public class PlayerController : MonoBehaviour
+	{
+		[SerializeField]
+		private float _raycastMaxDistance = 5;
+		[SerializeField]
+		private Camera _playerCamera;
+		[SerializeField]
+		private Slider _hitMarker;
+		[SerializeField]
+		private Image _hitMarkerImage;
+		private GameObject _gazedObject = null;
+		private float _markerFillingSpeed = 100;
+
+
+		void Update()
+		{
+			RaycastHit hit;
+
+			if (Physics.Raycast(_playerCamera.transform.position, _playerCamera.transform.forward, out hit, _raycastMaxDistance))
+			{
+				if (hit.collider.gameObject != _gazedObject)
+				{
+					SetNewObjectAsGazed(hit);
+				}
+				else
+				{
+					FillHitMarkerOnGazingSameObject();
+				}
+
+				if (hit.collider.tag == "TeleportPosition")
+				{
+					_hitMarkerImage.color = Color.green;
+					TeleportOnFullMarkerValue();
+				}
+				if (hit.collider.tag == "Enemies")
+				{
+					_hitMarkerImage.color = Color.red;
+					DestroyEnemyOnFullMarkerValue();
+				}
+			}
+			else
+			{
+				ResetOnTargetLose();
+			}
+		}
+
+
+		private void ResetOnTargetLose()
+		{
+			_gazedObject = null;
+			_hitMarker.value = 0;
+		}
+
+
+		private void SetNewObjectAsGazed(RaycastHit hit)
+		{
+			_gazedObject = hit.collider.gameObject;
+			_hitMarker.value = 0;
+		}
+
+
+		private void FillHitMarkerOnGazingSameObject()
+		{
+			_hitMarker.value += _markerFillingSpeed * Time.deltaTime;
+		}
+
+
+		private void TeleportOnFullMarkerValue()
+		{
+			if (_hitMarker.value == 100)
+			{
+				transform.position = _gazedObject.transform.position;
+			}
+		}
+
+
+		private void DestroyEnemyOnFullMarkerValue()
+		{
+			if (_hitMarker.value == 100)
+			{
+				Destroy(_gazedObject);
+			}
+		}
+	}
+}
